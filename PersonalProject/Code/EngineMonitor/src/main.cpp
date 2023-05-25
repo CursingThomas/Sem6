dcf28dc92186c383001cfb3d1940f52e9e54f039
Thomas <thschenk143@gmail.com>
2023-05-25 23:02:11 +0200

    Added code for pers

--- PersonalProject/Code/EngineMonitor/src/main.cpp
+++ PersonalProject/Code/EngineMonitor/src/main.cpp
@@ -6,8 +6,8 @@
 
 String URL = "http://i466372.hera.fhict.nl/connect.php";
 
-const char* ssid = "Interne"; 
-const char* password = "TLE9009tle"; 
+const char* ssid = "Test"; 
+const char* password = "Test1234"; 
 Reading sensorReading;
 
 const char* ntpServer = "pool.ntp.org";
@@ -15,6 +15,7 @@ const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
 struct tm timeinfo;
 RingBuf<Reading, 1000> buffer;
+int retriesConnection = 0;
 
 void printLocalTime()
 {
@@ -70,40 +71,85 @@ void setup() {
 }
 
 void loop() {
-  //new data has been gathered
-  buffer.push(sensorReading);
-  Serial.println(buffer.size());
   if (timeinfo.tm_year != 0)
+    {
+      printLocalTime();
+    }
+  if(WiFi.status() != WL_CONNECTED) 
   {
-    printLocalTime();
+    sensorReading.timeStamp = timeinfo;
+    buffer.push(sensorReading);
+     Serial.println(&sensorReading.timeStamp, "%A, %B %d %Y %H:%M:%S");
+    Serial.println(buffer.size());
+    Serial.println("No internet, data stored in buffer");
+    delay(1000);
   }
-  
-
   if(WiFi.status() == WL_CONNECTED) 
   {
-    String date = "0";
-    if (timeinfo.tm_year != 0)
-    {  
-      printLocalTime();
-      String year = String(timeinfo.tm_year);
-      year.remove(0,1);
-      date = ("20" + year + "-" + String((timeinfo.tm_mon + 1)) + "-" + String(timeinfo.tm_mday) + "_" + String(timeinfo.tm_hour) + ":" + String(timeinfo.tm_min) + ":" + String(timeinfo.tm_sec));
-    }
-    String postData = "oilTemperature=" + String(sensorReading.oilTemperature) + "&rpm=" + String(sensorReading.rpm) + "&speed=" + String(sensorReading.speed) + "&timestamp=" + date;
-    HTTPClient http;
-    http.begin(URL);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    int httpCode = http.POST(postData);
-    String payload = http.getString();
+    if (buffer.isEmpty() == false)
+    {
+      while(buffer.isEmpty() == false)
+      {
+        Reading tempObject;
+        buffer.pop(tempObject);
+        String date = "0";
+        if (tempObject.timeStamp.tm_year != 0)
+        {  
+          printLocalTime();
+          String year = String(tempObject.timeStamp.tm_year);
+          year.remove(0,1);
+          date = ("20" + year + "-" + String((tempObject.timeStamp.tm_mon + 1)) + "-" + String(tempObject.timeStamp.tm_mday) + "_" + String(tempObject.timeStamp.tm_hour) + ":" + String(tempObject.timeStamp.tm_min) + ":" + String(tempObject.timeStamp.tm_sec));
+        }
 
-    Serial.print("URL : "); Serial.println(URL); 
-    Serial.print("Data: "); Serial.println(postData);
-    Serial.print("httpCode: "); Serial.println(httpCode);
-    Serial.print("payload : "); Serial.println(payload);
-    Serial.println("--------------------------------------------------");  
+        String postData = "oilTemperature=" + String(tempObject.oilTemperature) + "&rpm=" + String(tempObject.rpm) + "&speed=" + String(tempObject.speed) + "&timestamp=" + date;
+        HTTPClient http;
+        http.begin(URL);
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        
+        int httpCode = http.POST(postData);
+        String payload = http.getString();
+
+        Serial.print("URL : "); Serial.println(URL); 
+        Serial.print("Data: "); Serial.println(postData);
+        Serial.print("httpCode: "); Serial.println(httpCode);
+        Serial.print("payload : "); Serial.println(payload);
+        Serial.println("--------------------------------------------------");
+      }
+
+    } else {
+      String date = "0";
+      if (timeinfo.tm_year != 0)
+      {  
+        printLocalTime();
+        String year = String(timeinfo.tm_year);
+        year.remove(0,1);
+        date = ("20" + year + "-" + String((timeinfo.tm_mon + 1)) + "-" + String(timeinfo.tm_mday) + "_" + String(timeinfo.tm_hour) + ":" + String(timeinfo.tm_min) + ":" + String(timeinfo.tm_sec));
+      }
+      String postData = "oilTemperature=" + String(sensorReading.oilTemperature) + "&rpm=" + String(sensorReading.rpm) + "&speed=" + String(sensorReading.speed) + "&timestamp=" + date;
+      HTTPClient http;
+      http.begin(URL);
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      
+      int httpCode = http.POST(postData);
+      String payload = http.getString();
+
+      Serial.print("URL : "); Serial.println(URL); 
+      Serial.print("Data: "); Serial.println(postData);
+      Serial.print("httpCode: "); Serial.println(httpCode);
+      Serial.print("payload : "); Serial.println(payload);
+      Serial.println("--------------------------------------------------");  
+    }
   }
 
   delay(1000);
-  
+
+  retriesConnection++;
+  if (retriesConnection >= 50)
+  {
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      connectWiFi();
+    }    
+    retriesConnection = 0;
+  }  
 }
